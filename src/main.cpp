aa9086fe8f736cd500b555b722fc6b96cd053bfd
lingannavinay <vinnu61614@gmail.com>
2018-12-09 15:05:18 +0000

    rewards updated

--- src/main.cpp
+++ src/main.cpp
@@ -2154,24 +2154,39 @@ double ConvertBitsToDouble(unsigned int nBits)
 
 int64_t GetBlockValue(int nHeight)
 {
+     int64_t nSubsidy = 0;
+
     if (nHeight == 0) {
-        return 17500000 * COIN;
-    } else if (nHeight > 0 && nHeight <= 200) {
-        return 2500 * COIN;
-    } else if (nHeight > 200 && nHeight <= 775600) {
-        return 7 * COIN;
-    } else if (nHeight > 775600 && nHeight <= 1043999) {
-        return 4.5 * COIN;
-    } else if (nHeight > 1043999 && nHeight <= 1562398) {
-        return 3.6 * COIN;
+        nSubsidy = 2500000000 * COIN;
+    } else if (nHeight > 0 && nHeight <= 525600) {
+        nSubsidy = 440 * COIN;
+    } else if (nHeight > 525600 && nHeight <= 1051200) {
+        nSubsidy = 330 * COIN;
+    } else if (nHeight > 1051200 && nHeight <= 1576800) {
+        nSubsidy = 220 * COIN;
+    } else if (nHeight > 1576800 && nHeight <= 2102400) {
+        nSubsidy = 110 * COIN;
+    } else if (nHeight > 2102400 && nHeight <= 2628000) {
+        nSubsidy = 55 * COIN;
     } else {
-        return 2.7 * COIN;
+        nSubsidy = 30 * COIN;
     }
+    return nSubsidy;
+
 }
 
 int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCount)
 {
-    int64_t ret = blockValue / 5 * 3;
+   LogPrintf("$$$ GetMasternodePayment method  nHeight ..%d\n",nHeight);
+		LogPrintf("$$$ GetMasternodePayment method blockValue  ..%d\n",blockValue);
+		
+        int64_t ret = 0;
+        if (nHeight >=0 && nHeight <= 525600) {
+        ret = blockValue / 20 * 13;
+    } else {
+                ret = blockValue / 5 * 3;
+        }
+
     return ret;
 }
 
