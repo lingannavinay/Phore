6660cecbebb740a026843964cf16a9bfa8218414
lingannavinay <vinnu61614@gmail.com>
2018-12-09 22:05:46 +0000

    just added comment

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -5,7 +5,7 @@
 // Copyright (c) 2017 The Phore developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+//Added gensis
 #include "libzerocoin/Params.h"
 #include "chainparams.h"
 #include "random.h"
